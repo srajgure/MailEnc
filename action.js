da1beba252e6658c5975ec2049267445303ef2ed
srajgure <sumit.rajgure123@gmail.com>
2020-02-20 19:37:51 +0530

    commit 4

--- action.js
+++ action.js
@@ -5,11 +5,13 @@ InboxSDK.load('1', 'sdk_app_check_ebe9d01b93').then(function(sdk){
 
 		// a compose view has come into existence, do something with it!
 		composeView.addButton({
-			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+			title: "MailEnc",
+			iconUrl: 'https://img.icons8.com/cotton/64/000000/lock.png',
 			onClick: function(event) {
+				var temp="hi there";
 				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-			},
+				//event.composeView.getText(temp);
+				console.log(temp);			},
 		});
 
 	});
