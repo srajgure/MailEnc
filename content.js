a3fcf7196ab5700aae651cd4cfb6a4762a2df307
srajgure <sumit.rajgure123@gmail.com>
2020-02-20 19:03:59 +0530

    Commit 1

---
+++ content.js
@@ -0,0 +1,17 @@
+InboxSDK.load('1', 'sdk_app_check_ebe9d01b93').then(function(sdk){
+
+	// the SDK has been loaded, now do something with it!
+	sdk.Compose.registerComposeViewHandler(function(composeView){
+
+		// a compose view has come into existence, do something with it!
+		composeView.addButton({
+			title: "My Nifty Button!",
+			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+			onClick: function(event) {
+				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+			},
+		});
+
+	});
+
+});
