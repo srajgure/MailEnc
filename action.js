95c5462e53cf85bc29d62c8e912abb149e44f862
srajgure <sumit.rajgure123@gmail.com>
2020-02-23 21:05:20 +0530

    commit 23/02

--- action.js
+++ action.js
@@ -1,17 +1,20 @@
 InboxSDK.load('1', 'sdk_app_check_ebe9d01b93').then(function(sdk){
 
-	// the SDK has been loaded, now do something with it!
+	
 	sdk.Compose.registerComposeViewHandler(function(composeView){
 
-		// a compose view has come into existence, do something with it!
+		
 		composeView.addButton({
 			title: "MailEnc",
 			iconUrl: 'https://img.icons8.com/cotton/64/000000/lock.png',
 			onClick: function(event) {
-				var temp="hi there";
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-				//event.composeView.getText(temp);
-				console.log(temp);			},
+				//Get Message from compose box.
+				var temp=event.composeView.getHTMLContent();
+				//var obj=new encryption(temp);
+				//Put encrpted message to compose box.
+				event.composeView.setBodyText(encrypt(temp));
+				//window.alert(encrypted);			
+			},
 		});
 
 	});
