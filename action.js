5b9adf6daa79190b3e7379570d54c6469ecf9d2c
AkshayWare007 <akshay.ware16@vit.edu>
2020-04-29 22:00:42 +0530

    Added RSA Encryption (Created new Button)

--- action.js
+++ action.js
@@ -4,11 +4,10 @@ InboxSDK.load('1', 'sdk_Akshay_0b1daa8634').then(function(sdk){
 	sdk.Compose.registerComposeViewHandler(function(composeView){
 		
 		composeView.addButton({
-			title: "MailEnc",
+			title: "AES-ENC",
 			iconUrl: 'https://img.icons8.com/cotton/64/000000/lock.png',
 			onClick: function(event) {
 				//Get Message from compose box.
-
                 do{
                 var key = prompt("Please enter your key:", "KEY");
 
@@ -27,13 +26,29 @@ InboxSDK.load('1', 'sdk_Akshay_0b1daa8634').then(function(sdk){
 			},
 		});
 
+		composeView.addButton({
+		    title: "RSA-ENC",
+            iconUrl: 'https://icons.iconseeker.com/png/fullsize/scrap/lock-6.png',
+            onClick: function(event) {
+                //Get Message from compose box.
+                var temp = event.composeView.getHTMLContent();
+
+                var N = '009afdccef68a56efc68669b498af90bd7cf3466646a853138a8e83d43072687be55575fd3dc15c0a39880389b07148dcbe175ab0f6e971b0638b69dad947d60fd';
+                var E = '0x010001';
+                //exponent
+                var encObj = new RSAKey();
+                encObj.setPublic(N,E);
+                var msg = encObj.encrypt(temp);
+                event.composeView.setBodyText(msg);
+            },
+		});
 	});
 
 	sdk.Conversations.registerMessageViewHandler(function(messageView){
 		
 		messageView.addToolbarButton({
 			section: sdk.Conversations.MessageViewToolbarSectionNames.MORE,
-			title: "Decrypt",
+			title: "Decrypt AES",
 			iconUrl: 'https://image.flaticon.com/icons/svg/2471/2471529.svg',
 			onClick: function(event) {
 					//Get Message from compose box.
@@ -61,5 +76,30 @@ InboxSDK.load('1', 'sdk_Akshay_0b1daa8634').then(function(sdk){
 					
 					},
 				});
+
+		messageView.addToolbarButton({
+		    section: sdk.Conversations.MessageViewToolbarSectionNames.MORE,
+            title: "Decrypt RSA",
+            iconUrl: 'https://icons.iconseeker.com/png/fullsize/scrap/lock-6.png',
+            onClick: function(event) {
+                    //Get Message from compose box.
+                    var cipher = messageView.getBodyElement().textContent;
+
+                    var N = '009afdccef68a56efc68669b498af90bd7cf3466646a853138a8e83d43072687be55575fd3dc15c0a39880389b07148dcbe175ab0f6e971b0638b69dad947d60fd';
+                    var D = '052b760c7e47165ea0f4db3526c78bd794f7c07a94fb005b4d7701a41cb6f24c1fae798447b8e82346ce47a53dcc26ed9fc37aa8508bc587040a64cf58fed999';
+                    var E = '0x010001';
+
+                    var encObj = new RSAKey();
+                    encObj.setPrivate(N,E,D);
+
+                    var plaintxt = encObj.decrypt(cipher);
+
+                    var opened = window.open("");
+                    var htmlstring = "<html><head><title>Decrypted text : </title></head><body>";
+                    htmlstring = htmlstring.concat(plaintxt);
+                    htmlstring = htmlstring.concat("</body></html>")
+                    opened.document.write(htmlstring);
+            },
+		});
 	});
 });
